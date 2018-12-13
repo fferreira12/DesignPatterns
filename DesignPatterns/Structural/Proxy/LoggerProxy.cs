20e0a3dc24884e3ce5b610c054733e3cded632b8
Fernando Rodrigues Ferreira <fferreira12@gmail.com>
2018-12-13 20:21:08 -0200

    Adds Proxy

---
+++ DesignPatterns/Structural/Proxy/LoggerProxy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Proxy
+{
+    class LoggerProxy : IService
+    {
+
+        IService serviceInstance = new RandomVowelService();
+
+        public string GetData()
+        {
+            string vowel = serviceInstance.GetData();
+            Console.WriteLine($"Client requested a Vowel and got a {vowel}");
+            return vowel;
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Proxy/RandomVowelService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Proxy
+{
+    class RandomVowelService : IService
+    {
+        private readonly string[] vowels = new string[] { "A", "E", "I", "O", "U" };
+        private Random random = new Random();
+
+        public string GetData()
+        {
+            return vowels[random.Next(vowels.Length)];
+        }
+    }
+}
